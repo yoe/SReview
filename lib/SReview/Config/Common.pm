f27c7e28b53d7654c5d8bbe439a48e80491a562d
Wouter Verhelst <w@uter.be>
2017-04-15 17:38:23 +0200

    Add more common configuration variables

--- lib/SReview/Config/Common.pm
+++ lib/SReview/Config/Common.pm
@@ -4,6 +4,10 @@ sub setup($) {
 	my $config = shift;
 
 	$config->define('dbistring', 'The DBI connection string used to connect to the database', 'dbi:Pg:dbname=sreview');
+	$config->define('raw_exten', 'The file extension of the raw recorded (untranscoded) file format', 'ts');
+	$config->define('workdir_mangle', 'A subroutine that returns an intermediate directory name based on the name of the room (first parameter) and the scheduled day of the talk (second parameter). Default returns just the room name.', sub { return $1; });
+	$config->define('workdir', 'The directory below which work-in-progress files are stored', '/srv/sreview');
+	$config->define('ffmpeg_a_codec', 'The audio codec (and its configuration) as used by ffmpeg for the raw recordings; needed for reintegrating the normalized audio', '-c:a libfdk_aac -b:a 128k');
 }
 
 1;
