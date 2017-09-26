5103b780e0b504fc714fb0d1441456f262de7dd7
Wouter Verhelst <w@uter.be>
2017-09-26 18:15:21 +0200

    Ensure that the image resolution is always correct

--- lib/SReview/Video/PNGGen.pm
+++ lib/SReview/Video/PNGGen.pm
@@ -1,6 +1,7 @@
 package SReview::Video::PNGGen;
 
 use Moose;
+use Carp;
 
 extends 'SReview::Video';
 
@@ -8,6 +9,9 @@ sub readopts {
 	my $self = shift;
 	my $output = shift;
 
+	if(defined($output->video_size) && ($self->video_size ne $output->video_size)) {
+		croak "Video resolution does not match image resolution";
+	}
 
 	return ('-loop', '1', '-framerate', $output->video_framerate, '-i', $self->url, '-f', 'lavfi', '-i', 'anullsrc');
 }
