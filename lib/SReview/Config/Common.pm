a9e274f1a11e5161bfe07b8d8e4b621f45b79e76
Wouter Verhelst <w@uter.be>
2017-11-09 12:27:13 +0100

    Migrate dispatch to new API, too
    While at it, rename to sreview-dispatch

--- lib/SReview/Config/Common.pm
+++ lib/SReview/Config/Common.pm
@@ -34,6 +34,7 @@ sub setup {
 	$config->define('workdir', 'A directory where encoder jobs can create a subdirectory for temporary files', '/tmp');
 	$config->define('outputdir', 'The base directory under which SReview should place the final released files', '/srv/sreview/output');
 	$config->define('output_subdirs', 'An array of fields to be used to create subdirectories under the output directory.', ['event', 'room', 'startdate']);
+	$config->define('script_output', 'The directory to which the output of scripts should be redirected', '/srv/sreview/script-output');
 	$config->define('preroll_template', 'An SVG template to be used as opening credits. Should have the same nominal dimensions (in pixels) as the video assets.', undef);
 	$config->define('postroll_template', 'An SVG template to be used as closing credits. Should have the same nominal dimensions (in pixels) as the video assets.', undef);
 	$config->define('postroll', 'A PNG file to be used as closing credits. Will only be used if no postroll_template was defined. Should have the same dimensions as the video assets.', undef);
@@ -44,6 +45,16 @@ sub setup {
 	$config->define('inputglob', 'A filename pattern (glob) that tells SReview where to find new files', '/srv/sreview/incoming/*/*/*');
 	$config->define('parse_re', 'A regular expression to parse a filename into year, month, day, hour, minute, second, and room', '.*\/(?<room>[^\/]+)\/(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})\/(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})');
 	$config->define('url_re', 'If set, used with parse_re in an s///g command to produce a URL', undef);
+
+	# Values for dispatch script
+	$config->define('state_actions', 'An anonymous hash that tells SReview what to do with a talk when it is in a given state. Can use embedded perl.', {
+		cutting => 'qsub -V -l input -l output -b y -pe smp 2 -N cut_<%== $talkid %> -o <%== $output_dir %> -e <%== $output_dir %> sreview-cut <%== $talkid %>',
+		generating_previews => 'qsub -V -l output -b y -N previews_<%== $talkid %> -o <%== $output_dir %> -e <%== $output_dir %> sreview-previews <%== $talkid %>',
+		transcoding => 'qsub -V -l output -b y -N transcode_<%== $talkid %> -o <%== $output_dir %> -e <%== $output_dir %> sreview-transcode <%== $talkid %>',
+		uploading => 'qsub -V -l output -b y -N upload_<%== $talkid %> -o <%== $output_dir %> -e <%== $output_dir %> sreview-skip <%== $talkid %>',
+		notification => 'qsub -V -l output -b y -N notify_<%== $talkid %> -o <%== $output_dir %> -e <%== $output_dir %> sreview-skip <%== $talkid %>',
+	});
+	$config->define('query_limit', 'A maximum number of jobs that should be submitted in a single loop in sreview-dispatch. 0 means no limit.', 0);
 
 	return $config;
 }
