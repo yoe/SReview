2c0cf03cf68c886b539a67d5444bbb1cd27eaedc
Wouter Verhelst <w@uter.be>
2018-02-01 14:54:29 +0100

    Updates
    - Drop the warning about the scaling thing, it doesn't work currently
      anyway (generates an illegal command line). We can fix this later (if
      we really want to).
    - Be a bit more explicit about the audio layout, so that hopefully
      ffmpeg doesn't trip over itself next.

--- lib/SReview/Video/PNGGen.pm
+++ lib/SReview/Video/PNGGen.pm
@@ -12,12 +12,8 @@ sub readopts {
 	if(defined($output->video_size) && ($self->video_size ne $output->video_size)) {
 		carp "Video resolution does not match image resolution. Will scale, but the result may be suboptimal...";
 
-		return ('-loop', '1', '-framerate', $output->video_framerate, '-i', $self->url, '-vf', 'scale=' . $output->video_size, '-f', 'lavfi', '-i', 'anullsrc');
 	}
-	if(defined($output->aspect_ratio) && ($self->aspect_ratio ne $output->aspect_ratio)) {
-		croak "Video aspect ratio does not match image aspect ratio";
-	}
-	return ('-loop', '1', '-framerate', $output->video_framerate, '-i', $self->url, '-f', 'lavfi', '-i', 'anullsrc');
+	return ('-loop', '1', '-framerate', $output->video_framerate, '-i', $self->url, '-f', 'lavfi', '-i', 'anullsrc=channel_layout=mono,r=' . $self->audio_samplerate);
 }
 
 no Moose;
