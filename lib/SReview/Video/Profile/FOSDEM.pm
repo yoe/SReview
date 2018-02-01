922c976daf4afc993530a054534b3e7ce659cec6
Wouter Verhelst <w@uter.be>
2018-02-01 21:28:04 +0100

    Fix syntax (whoops)

--- lib/SReview/Video/Profile/FOSDEM.pm
+++ lib/SReview/Video/Profile/FOSDEM.pm
@@ -1,10 +1,9 @@
 use SReview::Video::ProfileFactory;
-use SReview::Video::Profile::mp4;
 package SReview::Video::Profile::FOSDEM;
 
 use Moose;
 
-extends SReview::Video::Profile::mp4;
+extends 'SReview::Video::Profile::mp4';
 
 sub _probe_extra_params {
 	return { g => "45", "probesize" => "10M", "analyzeduration" => "10M" };
