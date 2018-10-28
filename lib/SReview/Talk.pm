2f8eb4e57a5e2c267dbd6ea130b539f27a80830b
Wouter Verhelst <w@uter.be>
2018-10-28 17:05:13 +0100

    Updates
    Too many to enumerate...

--- lib/SReview/Talk.pm
+++ lib/SReview/Talk.pm
@@ -2,7 +2,9 @@ package SReview::Talk;
 
 use Moose;
 use Mojo::Pg;
+use Mojo::Template;
 use SReview::Config::Common;
+use SReview::Talk::State;
 
 my $config = SReview::Config::Common::setup;
 my $pg = Mojo::Pg->new->dsn($config->get('dbistring')) or die "Cannot connect to database!";
@@ -24,9 +26,9 @@ sub _load_pathinfo {
 
 	my $pathinfo = {};
 
-	my $eventname = $pg->db->dbh->prepare("SELECT events.id AS eventid, events.name AS event, rooms.name AS room, rooms.outputname AS room_output, talks.starttime::date AS date, to_char(starttime, 'yyyy') AS year, talks.slug, talks.title, talks.subtitle FROM talks JOIN events ON talks.event = events.id JOIN rooms ON rooms.id = talks.room WHERE talks.id = ?");
-	$eventname->execute($self->talkid);
-	my $row = $eventname->fetchrow_hashref();
+	my $eventdata = $pg->db->dbh->prepare("SELECT events.id AS eventid, events.name AS event, rooms.name AS room, rooms.outputname AS room_output, rooms.id AS room_id, talks.starttime::date AS date, to_char(starttime, 'yyyy') AS year, talks.slug, talks.title, talks.subtitle, talks.state FROM talks JOIN events ON talks.event = events.id JOIN rooms ON rooms.id = talks.room WHERE talks.id = ?");
+	$eventdata->execute($self->talkid);
+	my $row = $eventdata->fetchrow_hashref();
 
 	$pathinfo->{"workdir"} = join('/', $row->{eventid}, $row->{date}, substr($row->{room}, 0, 1));
 
@@ -62,6 +64,18 @@ has 'eventname' => (
 sub _load_eventname {
 	my $self = shift;
 	return $self->_get_pathinfo->{raw}{event};
+}
+
+has 'state' => (
+	lazy => 1,
+	is => 'rw',
+	isa => 'SReview::Talk::State',
+	builder => '_load_state',
+);
+
+sub _load_state {
+	my $self = shift;
+	return SReview::Talk::State->new($self->_get_pathinfo->{raw}{state});
 }
 
 has 'title' => (
@@ -211,6 +225,35 @@ has 'room' => (
 
 sub _load_room {
 	return shift->_get_pathinfo->{raw}{room};
+}
+
+has 'roomid' => (
+	lazy => 1,
+	is => 'rw',
+	builder => '_load_roomid',
+);
+
+sub _load_roomid {
+	return shift->_get_pathinfo->{raw}{room_id}
+}
+
+has 'eventurl' => (
+	lazy => 1,
+	is => 'rw',
+	builder => '_load_eventurl',
+);
+
+sub _load_eventurl {
+	my $self = shift;
+	my $mt = Mojo::Template->new;
+	if(defined($config->get('eventurl_format'))) {
+		return $mt->render($config->get('eventurl_format'), {
+			slug => $self->slug,
+			room => $self->room,
+			date => $self->date,
+			year => $self->_get_pathinfo->{raw}{year}});
+	}
+	return "";
 }
 
 has 'preview_exten' => (
--- lib/SReview/Web/Controller/Review.pm
+++ lib/SReview/Web/Controller/Review.pm
@@ -4,6 +4,7 @@ use Mojo::Base 'Mojolicious::Controller';
 use Mojo::Collection 'c';
 
 use SReview::Talk;
+use SReview::Access qw/admin_for/;
 
 sub view {
 	my $c = shift;
@@ -15,10 +16,21 @@ sub view {
 	} else {
 		$talk = SReview::Talk->by_nonce($c->stash('nonce'));
 	}
+	my $variant;
+	if(admin_for($c, $talk) || $talk->state eq 'preview') {
+		$variant = undef;
+	} elsif($talk->state < 'preview') {
+		$variant = 'preparing';
+	} elsif($talk->state < 'done') {
+		$variant = 'transcode';
+	} else {
+		$variant = 'done';
+	}
+		
 	$c->stash(layout => 'default');
 	$c->stash(talk => $talk);
 	$c->stash(stylesheets => ['/review.css']);
-	$c->render;
+	$c->render(variant => $variant);
 }
 
 1;
