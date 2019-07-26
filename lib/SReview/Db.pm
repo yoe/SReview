yoe/SReview

commit b5dd59fcf4e2f7139446e985b7b7e24c77d477ef

Don't probe files if the mtime hasn't changed since last time we checked

lib/SReview/Db.pm
@@ -1026,6 +1026,10 @@ INSERT INTO properties(name) VALUES('offset_end');
 LOCK TABLE corrections IN SHARE MODE;
 DELETE FROM corrections USING properties WHERE corrections.property = properties.id AND properties.name = 'offset_end';
 DELETE FROM properties WHERE name = 'offset_end';
+-- 17 up
+ALTER TABLE talks ADD mtime INTEGER;
+-- 17 down
+ALTER TABLE talks DROP mtime;
 @@ code
 -- 1 up
 CREATE VIEW last_room_files AS

lib/SReview/Video.pm
@@ -88,6 +88,32 @@ has 'url' => (
 	required => 1,
 );
 
+=head2 mtime
+
+The mtime of the file backing this asset. Only defined if the file
+exists at the time the attribute is first read, and is not updated later
+on.
+
+=cut
+
+has 'mtime' => (
+	is => 'ro',
+	lazy => 1,
+	builder => '_probe_mtime',
+);
+
+sub _probe_mtime {
+	my $self = shift;
+	if($self->has_reference) {
+		return $self->reference->mtime;
+	}
+	my @statdata = stat($self->url);
+	if(length(@statdata) == 0) {
+		return undef;
+	}
+	return $statdata[9];
+}
+
 =head2 duration
 
 The duration of this asset.

scripts/sreview-detect
@@ -60,7 +60,7 @@ my $dbh = DBI->connect($config->get('dbistring'), '', '') or die "Cannot connect
 
 $dbh->begin_work;
 
-my $exists = $dbh->prepare("SELECT count(*) FROM raw_files WHERE filename = ?");
+my $exists = $dbh->prepare("SELECT mtime FROM raw_files WHERE filename = ?");
 my $add = $dbh->prepare("INSERT INTO raw_files(filename, room, starttime, endtime, stream) VALUES (?, (SELECT id FROM rooms WHERE altname = ? OR name = ?), ?::timestamptz, ?::timestamptz + ?::interval, ?)");
 my $update = $dbh->prepare("UPDATE raw_files SET endtime = starttime + ?::interval WHERE filename = ?");
 
@@ -84,12 +84,16 @@ foreach my $file(@files) {
 		$url =~ s/$parse_re/$url_build/g;
 	}
 	$exists->execute($url);
-	my $row = $exists->fetchrow_hashref;
-	my $length = SReview::Video->new(url => $url)->duration;
-        next unless defined($length);
-	if(defined($row->{count}) && $row->{count} eq '0') {
+	my $video = SReview::Video->new(url => $url);
+	if($exists->rows > 0) {
+		my $row = $exists->fetchrow_hashref;
+		my $mtime = $video->mtime;
+		next if($row->mtime != $mtime);
+		my $length = $video->duration;
+		next unless defined($length);
 		$add->execute($url, $room, $room, "$start_day $start_hour", "$start_day $start_hour", $length, $stream);
 	} else {
+		my $length = $video->duration;
 		$update->execute("$length", $url);
 	}
 }

commit 35abe75abfc6afdf814d96f34655de73b50edad8

Properly check for mtime changes
It's usually a good idea to test changes... :-/

Fixes: b5dd59fcf4e2f7139446e985b7b7e24c77d477ef

lib/SReview/Db.pm
@@ -1027,9 +1027,9 @@ LOCK TABLE corrections IN SHARE MODE;
 DELETE FROM corrections USING properties WHERE corrections.property = properties.id AND properties.name = 'offset_end';
 DELETE FROM properties WHERE name = 'offset_end';
 -- 17 up
-ALTER TABLE talks ADD mtime INTEGER;
+ALTER TABLE raw_files ADD mtime INTEGER;
 -- 17 down
-ALTER TABLE talks DROP mtime;
+ALTER TABLE raw_files DROP mtime;
 @@ code
 -- 1 up
 CREATE VIEW last_room_files AS

lib/SReview/Video.pm
@@ -108,7 +108,7 @@ sub _probe_mtime {
 		return $self->reference->mtime;
 	}
 	my @statdata = stat($self->url);
-	if(length(@statdata) == 0) {
+	if(scalar(@statdata) == 0) {
 		return undef;
 	}
 	return $statdata[9];

scripts/sreview-detect
@@ -61,8 +61,8 @@ my $dbh = DBI->connect($config->get('dbistring'), '', '') or die "Cannot connect
 $dbh->begin_work;
 
 my $exists = $dbh->prepare("SELECT mtime FROM raw_files WHERE filename = ?");
-my $add = $dbh->prepare("INSERT INTO raw_files(filename, room, starttime, endtime, stream) VALUES (?, (SELECT id FROM rooms WHERE altname = ? OR name = ?), ?::timestamptz, ?::timestamptz + ?::interval, ?)");
-my $update = $dbh->prepare("UPDATE raw_files SET endtime = starttime + ?::interval WHERE filename = ?");
+my $add = $dbh->prepare("INSERT INTO raw_files(filename, room, starttime, endtime, stream, mtime) VALUES (?, (SELECT id FROM rooms WHERE altname = ? OR name = ?), ?::timestamptz, ?::timestamptz + ?::interval, ?, ?)");
+my $update = $dbh->prepare("UPDATE raw_files SET endtime = starttime + ?::interval, mtime = ? WHERE filename = ?");
 
 my @files = glob($config->get('inputglob'));
 
@@ -84,17 +84,20 @@ foreach my $file(@files) {
 		$url =~ s/$parse_re/$url_build/g;
 	}
 	$exists->execute($url);
+	my $row = $exists->fetchrow_hashref;
 	my $video = SReview::Video->new(url => $url);
-	if($exists->rows > 0) {
-		my $row = $exists->fetchrow_hashref;
-		my $mtime = $video->mtime;
-		next if($row->mtime != $mtime);
+	my $mtime = $video->mtime;
+	if($exists->rows == 0) {
+		# Reading the length takes time, so we only do it if we
+		# really need to...
 		my $length = $video->duration;
 		next unless defined($length);
-		$add->execute($url, $room, $room, "$start_day $start_hour", "$start_day $start_hour", $length, $stream);
+		$add->execute($url, $room, $room, "$start_day $start_hour", "$start_day $start_hour", $length, $stream, $mtime);
 	} else {
+		next unless ($row->{mtime} != $mtime);
 		my $length = $video->duration;
-		$update->execute("$length", $url);
+		next unless defined($length);
+		$update->execute("$length", $mtime, $url);
 	}
 }
 