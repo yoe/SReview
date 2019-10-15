83e8eb038eb22bb9738b56c5a7a16857fa686f7a
Wouter Verhelst <w@uter.be>
2019-10-16 00:51:34 +0200

    Use the Mojo::UserAgent API correctly

--- dockerfiles/tests/test.pl
+++ dockerfiles/tests/test.pl
@@ -10,16 +10,16 @@ use Data::Dumper;
 
 my $ua = Mojo::UserAgent->new;
 
-my $res = $ua->post(join('/', $ENV{BASEURL}, 'login_post' => form => { email => $ENV{SREVIEW_ADMINUSER}, pass => $ENV{SREVIEW_ADMINPW} }));
+my $res = $ua->post(join('/', $ENV{BASEURL}, 'login_post' => form => { email => $ENV{SREVIEW_ADMINUSER}, pass => $ENV{SREVIEW_ADMINPW} }))->result;
 
 $res->is_success or die "error " . $res->code . ": " . $res->message;
 
 my $talk = SReview::Talk->new(talkid => 1);
 
-$res = $ua->get(join('/', $ENV{BASEURL}, $talk->nonce, 'data'));
+$res = $ua->get(join('/', $ENV{BASEURL}, $talk->nonce, 'data'))->result;
 
 $res->is_success or die "error " . $res->code . ": " . $res->message;
 
-my $json = decode_json($res->result->body);
+my $json = decode_json($res->body);
 
 print Dumper($json);
