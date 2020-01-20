5bad52f82978d76857e1698c6deeb48bd53e60a0
Wouter Verhelst <w@uter.be>
2020-01-20 19:01:42 +0200

    Dereference the S3 auth stuff

--- .ci/setup-minio.pl
+++ .ci/setup-minio.pl
@@ -11,6 +11,6 @@ exit 0 unless exists($ENV{SREVIEWTEST_BUCKET});
 
 my $config = decode_json($ENV{SREVIEWTEST_S3_CONFIG});
 
-my $s3 = Net::Amazon::S3->new($config);
+my $s3 = Net::Amazon::S3->new($config->{default});
 
 $s3->add_bucket({bucket => $ENV{SREVIEWTEST_BUCKET}});
