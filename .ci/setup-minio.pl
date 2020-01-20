36a8274afed7ca4e39ab5fefeca53c408375fa07
Wouter Verhelst <w@uter.be>
2020-01-20 18:36:23 +0200

    Also test the S3 access stuff using minio

---
+++ .ci/setup-minio.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Net::Amazon::S3;
+use Mojo::JSON;
+
+exit 0 unless exists($ENV{SREVIEWTEST_S3_CONFIG});
+exit 0 unless exists($ENV{SREVIEWTEST_BUCKET});
+
+my $config = decode_json($ENV{SREVIEWTEST_S3_CONFIG});
+
+my $s3 = Net::Amazon::S3->new($config);
+
+$s3->add_bucket({bucket => $ENV{SREVIEWTEST_BUCKET}});
