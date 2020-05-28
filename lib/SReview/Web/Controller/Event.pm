538ea3010fca712377fcd5372fd6d9de044b08c9
Wouter Verhelst <w@uter.be>
2020-05-28 15:47:52 +0200

    Implement 'add_with_json' API helper, too
    The 'json_populate_record' hack doesn't work, as it wants to use NULL
    rather than leaving things out, which results in the primary key not
    being autogenerated due to it being specified as NULL, and we don't want
    that.

--- lib/SReview/Web/Controller/Event.pm
+++ lib/SReview/Web/Controller/Event.pm
@@ -1,7 +1,7 @@
 package SReview::Web::Controller::Event;
 
 use Mojo::Base 'Mojolicious::Controller';
-use SReview::API::Helpers qw/db_query update_with_json/;
+use SReview::API::Helpers qw/db_query update_with_json add_with_json/;
 use Data::Dumper;
 
 my %fields = (
@@ -14,11 +14,7 @@ my %fields = (
 sub add {
 	my $c = shift->openapi->valid_input or return;
 
-	my $event = $c->req->body;
-
-	$c->app->log->debug("adding event");
-
-	$c->render(openapi => db_query($c->dbh, "INSERT INTO events(select * FROM json_populate_record(null::events, ?)) RETURNING id", $event));
+	return add_with_json($c, $c->req->json, "events", \%fields);
 }
 
 sub update {
