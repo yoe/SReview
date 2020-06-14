479e8eb93eddf7872c605f6e5769f8e4c3a04307
Wouter Verhelst <w@uter.be>
2020-06-14 14:52:40 +0200

    Fix access to webinterface

--- dockerfiles/tests/test.pl
+++ dockerfiles/tests/test.pl
@@ -5,18 +5,24 @@ use warnings;
 
 use Mojo::UserAgent;
 use Mojo::JSON qw/decode_json/;
+use Mojo::URL;
 use SReview::Talk;
+use SReview::Config::Common;
 use Data::Dumper;
 
 my $ua = Mojo::UserAgent->new;
 
-my $res = $ua->post(join('/', $ENV{BASEURL}, 'login_post' => form => { email => $ENV{SREVIEW_ADMINUSER}, pass => $ENV{SREVIEW_ADMINPW} }))->result;
+my $config = SReview::Config::Common::setup();
 
-$res->is_success or die "error " . $res->code . ": " . $res->message;
+my $baseurl = Mojo::URL->new($config->get('urlbase'));
+
+my $res = $ua->post(Mojo::URL->new("/login_post")->base($baseurl)->to_abs => form => { email => $config->get('adminuser'), pass => $config->get('adminpw') })->result;
+
+$res->is_redirect or die "error " . $res->code . ": " . $res->message;
 
 my $talk = SReview::Talk->new(talkid => 1);
 
-$res = $ua->get(join('/', $ENV{BASEURL}, $talk->nonce, 'data'))->result;
+$res = $ua->get(Mojo::URL->new("/r/" . $talk->nonce. "/data")->base($baseurl)->to_abs)->result;
 
 $res->is_success or die "error " . $res->code . ": " . $res->message;
 
