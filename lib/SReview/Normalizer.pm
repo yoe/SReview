3c4096acc6e64471472a7f3ffee937b40825750d
Wouter Verhelst <w@uter.be>
2020-08-07 16:10:30 +0200

    Fix whitespace

--- lib/SReview/Normalizer.pm
+++ lib/SReview/Normalizer.pm
@@ -141,7 +141,7 @@ sub run {
 	push @command, $self->audio->url;
 	print "Running: '" . join("' '", @command) . "'\n";
 	system(@command);
-	my $audio_in = SReview::Video->new(url => join('/', $self->tempdir, basename($fullname, [ ".wav" ])) . ".flac");
+	my $audio_in = SReview::Video->new(url => join('/', $self->tempdir, basename($filename, [ ".wav" ])) . ".flac");
 	my $map_v = SReview::Map->new(input => $self->input, type => "stream", choice => "video");
 	my $map_a = SReview::Map->new(input => $audio_in, type => "stream", choice => "audio");
 	$self->output->audio_codec($self->audio_codec);
