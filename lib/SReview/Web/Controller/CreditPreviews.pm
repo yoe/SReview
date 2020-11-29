7b3839b69730ff77dbb9fbff23bb795d59b24014
Wouter Verhelst <w@uter.be>
2020-11-29 15:01:37 +0200

    Get the template from the stash, rather than hardcoding it

--- lib/SReview/Web/Controller/CreditPreviews.pm
+++ lib/SReview/Web/Controller/CreditPreviews.pm
@@ -10,12 +10,13 @@ sub serve_png {
 	my $c = shift->openapi->valid_input;
 	my $slug = $c->param("slug");
 	my $suffix = $c->stash("suffix");
+	my $template = $c->stash("template");
 	my $talk = SReview::Talk->by_slug($slug);
 	my $input_coll = SReview::Files::Factory->create("intermediate", $c->srconfig->get("pubdir"));
 	my $relname = $talk->relative_name . $suffix;
 	if($c->param("force") || !($input_coll->has_file($relname))) {
 		my $preroll_file = $input_coll->add_file(relname => $relname);
-		process_template($c->srconfig->get('preroll_template'), $preroll_file->filename, $talk, $c->srconfig);
+		process_template($c->srconfig->get($template), $preroll_file->filename, $talk, $c->srconfig);
 		$preroll_file->store_file;
 	}
 	return $c->reply->file($input_coll->get_file(relname => $relname)->filename);
