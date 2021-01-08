cf7b0e2a92ef56af639487f9b7a744f2ba9bce84
Wouter Verhelst <w@uter.be>
2021-01-08 16:55:24 +0200

    Be clearer when the upload is being processed.

--- lib/SReview/Web/Controller/Review.pm
+++ lib/SReview/Web/Controller/Review.pm
@@ -37,6 +37,8 @@ sub view {
 		$variant = 'preparing';
 	} elsif($talk->state < 'done') {
 		$variant = 'transcode';
+	} elsif($talk->state = 'injecting') {
+		$variant = 'injecting';
 	} else {
 		$variant = 'done';
 	}
