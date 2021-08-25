234e763f88ab61f8780486d05ca22f8607dcbf61
Wouter Verhelst <w@uter.be>
2021-08-25 13:03:58 +0200

    Broken talks are also reviewable

--- lib/SReview/Web/Controller/Event.pm
+++ lib/SReview/Web/Controller/Event.pm
@@ -64,7 +64,7 @@ sub overview {
 	}
 
 	if($c->srconfig->get("anonreviews")) {
-		$query = "SELECT CASE WHEN state='preview' THEN '/r/' || nonce WHEN state='finalreview' THEN '/f/' || nonce ELSE null END AS reviewurl, nonce, name, speakers, room, starttime::timestamp, endtime::timestamp, state, progress FROM talk_list WHERE eventid = ? AND state IS NOT NULL ORDER BY state, progress, room, starttime";
+		$query = "SELECT CASE WHEN state IN ('preview', 'broken') THEN '/r/' || nonce WHEN state='finalreview' THEN '/f/' || nonce ELSE null END AS reviewurl, nonce, name, speakers, room, starttime::timestamp, endtime::timestamp, state, progress FROM talk_list WHERE eventid = ? AND state IS NOT NULL ORDER BY state, progress, room, starttime";
 	} else {
 		$query = "SELECT name, speakers, room, starttime::timestamp, endtime::timestamp, state, progress FROM talk_list WHERE eventid = ? AND state IS NOT NULL ORDER BY state, progress, room, starttime";
 	}
