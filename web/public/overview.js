cbc0e92bd71cc934c162c6c6191af31c36cb75da
Wouter Verhelst <w@uter.be>
2022-07-24 20:19:01 +0200

    Drop superfluous extra function

--- web/public/overview.js
+++ web/public/overview.js
@@ -1,12 +1,3 @@
-function updated(vm) {
-  if(vm.event !== vm.last_event) {
-    fetch("/api/v1/event/" + vm.event + "/overview")
-    .then(response => response.json())
-    .then((data) => {vm.rows = data; vm.last_event = vm.event})
-    .catch(error => console.error(error));
-  }
-};
-
 var vm = new Vue({
   el: '#overview',
   data: {
@@ -24,17 +15,22 @@ var vm = new Vue({
       .catch(error => console.error(error));
     }
   },
+  updated: function() {
+    if(this.event !== this.last_event) {
+      fetch("/api/v1/event/" + this.event + "/overview")
+      .then(response => response.json())
+      .then((data) => {this.rows = data; this.last_event = this.event})
+      .catch(error => console.error(error));
+    }
+  },
   created: function() {
     fetch("/api/v1/config")
     .then(response => response.json())
-    .then(data => {this.event = data.event; updated(this);})
+    .then(data => {this.event = data.event; this.updated;})
     .catch(error => console.error(error));
     fetch("/api/v1/event/list")
     .then(response => response.json())
     .then(data => {this.events = data;})
     .catch(error => console.error(error));
-  },
-  updated: function() {
-    updated(this);
   }
 })
