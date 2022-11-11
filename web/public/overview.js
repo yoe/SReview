ee658e16f0830fd3e3af84c87f7927dcbc91ade7
Stefano Rivera <stefanor@debian.org>
2022-11-11 16:45:49 +0200

    Free-text search

--- web/public/overview.js
+++ web/public/overview.js
@@ -5,11 +5,36 @@ const load_event = function() {
   .catch(error => console.error(error));
 };
 
+const search_text = function(search_terms, talk) {
+  const needle = search_terms.toLowerCase();
+  for (field of ['name', 'speakers']) {
+    const value = talk[field];
+    if (needle === undefined) {
+      continue;
+    }
+    if (value.toLowerCase().includes(needle)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const filter_talks = function() {
+  vm.rows = vm.talks.filter(talk => {
+    if (vm.search && ! search_text(vm.search, talk)) {
+      return false;
+    }
+    return true;
+  });
+}
+
 var vm = new Vue({
   el: '#overview',
   data: {
     title: "",
-    rows: [],
+    search: "",
+    talks: [],  // unfiltered
+    rows: [],   // filtered
     events: [],
     event: undefined,
     expls: []
@@ -21,6 +46,8 @@ var vm = new Vue({
   },
   watch: {
     event: load_event,
+    search: filter_talks,
+    talks: filter_talks,
   },
   created: function() {
     fetch("/api/v1/config")
