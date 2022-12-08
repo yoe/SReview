253923bf152da494f70aab181365ab72fac5541b
Wouter Verhelst <w@uter.be>
2022-12-08 12:14:09 +0200

    run-kube: update to be more flexible
    Previously, we would just create a Job and expect kubernetes to not
    start things if there aren't enough resources.
    
    This is not too flexible, and can create issues with other services
    running in the same cluster. So, allow for reconfiguring how things are
    run inside kubernetes.
    
    Add two example run-kube config files; one using the old system, one
    using kubequeue.

--- dockerfiles/master-kube/config.pm
+++ dockerfiles/master-kube/config.pm
@@ -1,11 +1,13 @@
 $state_actions = {
   'announcing' => 'run-kube announce <%== $talkid %>',
+  'notify_final' => 'run-kube notify_final <%== $talkid %>',
   'cutting' => 'run-kube cut <%== $talkid %>',
   'generating_previews' => 'run-kube previews <%== $talkid %>',
   'notification' => 'run-kube notify <%== $talkid %>',
   'transcoding' => 'run-kube transcode <%== $talkid %>',
   'uploading' => 'run-kube upload <%== $talkid %>',
   'injecting' => 'run-kube inject-job <%== $talkid %>',
+  'remove' => 'run-kube remove <%== $talkid %>',
 };
 
 1;
